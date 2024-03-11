f85ee717c55b9a88045a5c8e6c8ce80bb1b7f732
nicoleineza <nicole.ineza@ashesi.edu.gh>
2024-03-11 23:25:56 +0000

    improved dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,95 +1,165 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="chore icon.png">
     <title>GRINGOTTS</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
-    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="/bookcatalogue_/css/dashboard.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
+
+
 <body>
-    <input type="checkbox" id="nav-toggle">
-    <div class="sidebar">
-        <div class="sidebar-brand">
-            <h2><span class="las la-book"></span> Book Catalogue</h2>
+<div class="sidebar">
+    <div class="logo">
+        <!--<img src="" width="100" height="100">-->
+    </div>
+
+
+    <ul class="menu">
+        <li>
+            <a href="dashboard.php" class="active">
+                <i class='bx bx-home'></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="library.php">
+                <i class='bx bx-book'></i>
+                <span>Book Catalogue</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-book-reader'></i>
+                <span>Goals</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-medal'></i>
+                <span>Competitions</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-search'></i>
+                <span>Search</span>
+            </a>
+        </li>
+        <li>
+            <a href="discover.php">
+                <i class='bx bx-compass'></i>
+                <span>Discover</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-user'></i>
+                <span>Profile</span>
+            </a>
+        </li>
+
+
+        <li class="logout">
+            <a href="login.php">
+                <i class='bx bx-log-out'></i>
+                <span>Sign out</span>
+            </a>
+        </li>
+
+
+    </ul>
+
+
+</div>
+
+
+<div class="main">
+    <div class="header-wrapper">
+        <div class="header-title">
+            <span>Welcome to</span>
+            <h2>User Dashboard</h2>
         </div>
-        <div class="sidebar-menu">
+
+
+        <div class="user-info">
+            <div class="user-details">
+                <img src="user-image.jpg" alt="User Image" width="40px" height="40px"><br>
+                <span class="user-name">User Name</span>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="statistics-container">
+        <h3 class="main-title">Activity</h3>
+        <div class="statistics-wrapper">
+            <a href="managechores.html" style="text-decoration: none;">
+                <div class="progress">
+                    <div class="progress-header">
+                        <div class="stats-value">
+                            <span class="title"><strong><i class='bx bx-line-chart'></i> Progress</strong></span>
+                        </div>
+                        <span class="value"><strong>5/10</strong><br>Way to Go!</span>
+                    </div>
+                </div>
+            </a>
+
+
+            <a href="goals.php" style="text-decoration: none;">
+                <div class="achievements">
+                    <div class="achievements-header">
+                        <div class="achievements-value">
+                            <span class="title"><strong><i class='bx bx-medal'></i>Achievements</strong></span>
+                        </div>
+                        <span class="value"><strong>0</strong></span>
+                    </div>
+                </div>
+            </a>
+
+
+            <a href="competitions.php" style="text-decoration: none;">
+                <div class="mywork">
+                    <div class="mywork-header">
+                        <div class="mywork-value">
+                            <span class="title"><strong><i class='bx bx-task'></i>My Work</strong></span>
+                        </div>
+                        <span class="value"><strong>0</strong></span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="footer_container">
+        <div class="footer_logo">
+            <a href="#"></a>
+        </div>
+        <div class="footer_links">
             <ul>
-                <li><a href="dashboard.php" class="active"><span class="las la-home"></span> Dashboard</a></li>
-                <li><a href="library.php"><span class="las la-book"></span> Book Catalogue</a></li>
-                <li><a href="#"><span class="las la-book-reader"></span> Reading Goals</a></li>
-                <li><a href="#"><span class="las la-medal"></span> Competitions</a></li>
-                <li><a href="search.php"><span class="las la-search"></span> search</a></li>
-                <li><a href="discover.php"><span class="las la-compass"></span> Discover</a></li>
-                <li><a href="#"><span class="las la-user"></span> Profile</a></li>
-                <li><a href="login.php"><span class="las la-sign-out-alt"></span> Sign out</a></li>
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fas fa-times"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
             </ul>
         </div>
     </div>
-    <div class="main-content">
-        <header>
-            <h2>
-                <label for="nav-toggle">
-                    <span class="las la-bars"></span>
-                </label>
-                Dashboard
-            </h2>
-            <div class="user-wrapper">
-                <img src="/assets/icons/user_icon.jpg" width="40px" height="40px" alt="">
-                <div>
-                    <h4>Someone Someone</h4>
-                    <small>user</small>
-                </div>
-            </div>
-        </header> 
-        <main>
-            <div class="cards">
-                <div class="card-single">
-                    <div>
-                        <h1>5/10</h1>
-                        <span>progress</span>
-                        <h5>Way to Go!</h5>
-                    </div>
-                    <div>
-                        <span class="las la-chart-line"></span>
-                    </div>
-                </div>
-                <div class="card-single">
-                    <div>
-                        <h1>7</h1>
-                        <span>Achievements</span>
-                    </div>
-                    <div>
-                        <span class="las la-trophy"></span>
-                    </div>
-                </div>
-                <div class="card-single">
-                    <div>
-                        <h1>7</h1>
-                        <span>My Work</span>
-                    </div>
-                    <div>
-                        <span class="las la-pencil-alt"></span>
-                    </div>
-                </div>
-                <div class="card-single">
-                    <div>
-                        <h1>124</h1>
-                        <span>Profile Views</span>
-                    </div>
-                    <div>
-                        <span class="las la-user"></span>
-                    </div>
-                </div>
-                
-                
-            </div>
-            <div class="recent-flex">
-                
-            </div>
-        </main>
+    <div id="footer_social">
+       
     </div>
-    
+    <div class="footer_bottom">
+        <p>&copy; 2024 GRINGOTTS. All rights reserved.</p>
+    </div>
+</footer>
+
+
 </body>
 </html>
+
+
+
