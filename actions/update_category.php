c2419d5c1f7a000c4917a683e53c878129210f17
Anne Alwala <anne.alwala@ashesi.edu.gh>
2024-03-18 12:12:01 +0000

    new file

---
+++ actions/update_category.php
@@ -0,0 +1,28 @@
+<?php
+include '../settings/connection.php';
+$userID = 1;
+
+// Get the data from the AJAX request
+
+$categoryID = $_POST['categoryID'];
+$isChecked = $_POST['isChecked'];
+$bookID = $_POST['bookID'];
+
+// Prepare the query
+if ($isChecked === 'true') {
+    $query = "INSERT INTO bookcategories (UserID, BookID, CategoryID) VALUES (?, ?, ?)";
+} else {
+    $query = "DELETE FROM bookcategories WHERE UserID = ? AND BookID = ? AND CategoryID = ?";
+}
+
+$stmt = $db->prepare($query);
+$stmt->bind_param("iii", $userID, $bookID, $categoryID);
+
+// Execute the query
+if ($stmt->execute()) {
+    echo "Category updated successfully";
+} else {
+    echo "Error updating category: " . $stmt->error;
+}
+
+$stmt->close();
